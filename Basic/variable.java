2160763eeb2c8894ac9a4ebf9f1c0ff1e89dd8bf
santosh302 <santoshkumarsy05@gmail.com>
2024-02-11 22:25:41 +0530

    varibles

---
+++ Basic/variable.java
@@ -0,0 +1,47 @@
+public class variable {
+    public static void main(String[] args){
+
+        //print some things
+        System.out.println("Hello Santosh yadav Welcome to java dsa Course");
+        System.out.println(524);
+        System.out.println("Will be in same line as 524");
+
+        //look at some variable
+        //int age; ->just declare
+        int age = 24; //declare & initalise
+        System.out.println(age); //print 24
+        age = 25;
+        System.out.println(age); //print 25
+
+        short sample_num = 10000;
+        System.out.println(sample_num);
+
+        long sample_long_num= 100000000000l;
+        System.out.println(sample_long_num);
+
+         //decimal numbers
+         double dec_num_1 =51.4;
+         System.out.println(dec_num_1);
+
+         //decimal numbers
+         float dec_num_2 = 51.4f;
+         System.out.println(dec_num_2);
+
+         //boolean
+         boolean val= true;
+         System.out.println(val);
+
+         //character 
+         char char_var= '$';
+         System.out.println(char_var);
+
+         //type conversion
+         long var1 = 15;
+         int var2= 30;
+         var1 =var2 ; //int (30) is automatically converted to long
+         System.out.println(var1);
+
+    
+    }
+    
+}
